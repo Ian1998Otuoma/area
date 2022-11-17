4fd9c53b38f3b2d61579802f14ded594ec21bdae
Ian1998Otuoma <115020729+Ian1998Otuoma@users.noreply.github.com>
2022-11-18 02:26:07 +0300

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //declaration
+    int base,height,hypotenuse;
+    int area,perimeter;
+ 
+    hypotenuse=12;
+    //workings
+    area=0.5*base*height;
+    perimeter=base+height+hypotenuse;
+    //initilization
+    cout<<"The area of the triangle is:"<<area<<endl;
+    cout<<"The perimeter of the triangle:"<<perimeter<endl;
+     
+     return 0;
+
+}
